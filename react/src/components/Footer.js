0e540627b2c4955fd6344550ea2e46c8900232e0
Peter Solař <ptrslr@piotr-mbp.local>
2017-05-14 22:50:45 +0200

    First commit

---
+++ react/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="Footer pv3 tc">
+                <div className="Container">
+                    Copyright
+                </div>
+            </footer>
+        );
+    }
+}
+export default Footer;
---
+++ react/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './../tachyons/css/tachyons.min.css';
+import './css/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
