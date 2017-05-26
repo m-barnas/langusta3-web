81001288c6174cba85c3ef54b722402424e05bec
Martin Barnáš <xbarnas@fi.muni.cz>
2017-05-26 17:11:22 +0200

    persistence module updated

--- persistence/src/main/java/cz/fi/muni/PB138/entity/Word.java
+++ persistence/src/main/java/cz/fi/muni/PB138/entity/Word.java
@@ -18,24 +18,124 @@ import javax.validation.constraints.NotNull;
 public class Word {
 
     @Id
-    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
+    @Column(nullable = false)
     private String infinitive;
 
     private String pattern;
 
     @NotNull
+    @Column(name = "declined_value", nullable = false)
     private String declinedValue;
 
-
+    @Enumerated(EnumType.STRING)
+    @Column(name = "word_class")
     private WordClass wordClass;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "grammatical_gender")
     private GrammaticalGender grammaticalGender;
 
+    @Enumerated(EnumType.STRING)
     private Number number;
 
-    private GrammaticalCase grammaticaCase;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "grammatical_case")
+    private GrammaticalCase grammaticalCase;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInfinitive() {
+        return infinitive;
+    }
+
+    public void setInfinitive(String infinitive) {
+        this.infinitive = infinitive;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getDeclinedValue() {
+        return declinedValue;
+    }
+
+    public void setDeclinedValue(String declinedValue) {
+        this.declinedValue = declinedValue;
+    }
+
+    public WordClass getWordClass() {
+        return wordClass;
+    }
+
+    public void setWordClass(WordClass wordClass) {
+        this.wordClass = wordClass;
+    }
+
+    public GrammaticalGender getGrammaticalGender() {
+        return grammaticalGender;
+    }
+
+    public void setGrammaticalGender(GrammaticalGender grammaticalGender) {
+        this.grammaticalGender = grammaticalGender;
+    }
+
+    public Number getNumber() {
+        return number;
+    }
+
+    public void setNumber(Number number) {
+        this.number = number;
+    }
+
+    public GrammaticalCase getGrammaticalCase() {
+        return grammaticalCase;
+    }
+
+    public void setGrammaticalCase(GrammaticalCase grammaticalCase) {
+        this.grammaticalCase = grammaticalCase;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Word)) return false;
+
+        Word word = (Word) o;
+
+        return getId().equals(word.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "id=" + id +
+                ", infinitive='" + infinitive + '\'' +
+                ", pattern='" + pattern + '\'' +
+                ", declinedValue='" + declinedValue + '\'' +
+                ", wordClass=" + wordClass +
+                ", grammaticalGender=" + grammaticalGender +
+                ", number=" + number +
+                ", grammaticalCase=" + grammaticalCase +
+                '}';
+    }
 }
