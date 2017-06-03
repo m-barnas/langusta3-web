48cd37067d26c974487eaeae15ffcc00aee34ca0
ptrslr <solarptr@gmail.com>
2017-06-03 11:08:18 +0200

    Styles

--- react/src/components/AnalysisData.js
+++ react/src/components/AnalysisData.js
@@ -7,26 +7,26 @@ class AnalysisData extends Component {
 
   render() {
     return (
-      <table className="collapse w-50 tl center">
+      <table className="collapse w-50 tl">
         <tr>
-          <th className="w-20 v-top pb2 normal ">Pád:</th>
-          <td className="w-80 v-top pb2 b dark-gray">nominatív</td>
+          <th className="w-30 v-top pv1 normal ">Pád:</th>
+          <td className="w-70 v-top pv1 b dark-gray">nominatív</td>
         </tr>
         <tr>
-          <th className="w-20 v-top pb2 normal ">Rod:</th>
-          <td className="w-80 v-top pb2 b dark-gray">mužský</td>
+          <th className="w-30 v-top pv1 normal ">Rod:</th>
+          <td className="w-70 v-top pv1 b dark-gray">mužský</td>
         </tr>
         <tr>
-          <th className="w-20 v-top pb2 normal ">Číslo:</th>
-          <td className="w-80 v-top pb2 b dark-gray">singulár</td>
+          <th className="w-30 v-top pv1 normal ">Číslo:</th>
+          <td className="w-70 v-top pv1 b dark-gray">singulár</td>
         </tr>
         <tr>
-          <th className="w-20 v-top pb2 normal ">Infinitiv:</th>
-          <td className="w-80 v-top pb2 b dark-gray">huh</td>
+          <th className="w-30 v-top pv1 normal ">Infinitiv:</th>
+          <td className="w-70 v-top pv1 b dark-gray">huh</td>
         </tr>
         <tr>
-          <th className="w-20 v-top pb2 normal ">Vzory:</th>
-          <td className="w-80 v-top pb2 b dark-gray">
+          <th className="w-30 v-top pv1 normal ">Vzory:</th>
+          <td className="w-70 v-top pv1 b dark-gray">
             <ul className="list pl0 mv0">
               <li><a className="link underline-hover black" href="#">dub</a></li>
               <li><a className="link underline-hover black" href="#">stroj</a></li>
