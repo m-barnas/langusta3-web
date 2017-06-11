51e49f49e2c3581eca99db9bdfd5fcb9918922d7
ptrslr <solarptr@gmail.com>
2017-06-11 22:00:43 +0200

    Fix patternContainer deletion

--- react/src/App.js
+++ react/src/App.js
@@ -8,7 +8,7 @@ import PatternSearchContainer from './containers/PatternSearchContainer';
 import PatternCountSearchContainer from './containers/PatternCountSearchContainer';
 import AboutContainer from './containers/AboutContainer';
 import ContactContainer from './containers/ContactContainer';
-import PatternContainer from './containers/PatternContainer';
+// import PatternContainer from './containers/PatternContainer';
 
 import './../tachyons/css/tachyons.css';
 import './css/App.css';
